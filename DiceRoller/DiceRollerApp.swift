3c20e91201f51bd6feb0c296e62e271e87cad067
Al <asavicheva@internet.ru>
2026-03-12 21:31:23 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Александра Савичева on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
